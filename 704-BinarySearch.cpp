77da44ec8f59811fc11f87b6fe32bbcd78f671de
Rimon <mdraisul.i.rimon@gmail.com>
2025-04-06 08:21:27 +0600

    https://leetcode.com/problems/binary-search/description/

--- 704-BinarySearch.cpp
+++ 704-BinarySearch.cpp
@@ -4,35 +4,57 @@ using namespace std;
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int left=0,right=nums.size()-1,mid;
-
-        while(left<=right)
+        int low=0;
+        int high=nums.size()-1;
+        while(low<=high)
         {
-            mid=left+(right-left)/2;
+            int mid=(low+high)/2;
 
             if(nums[mid]==target)
             {
                 return mid;
             }
-            else if(nums[mid]<target)
-            {
-                left=mid+1;
+            else if(nums[mid]<target){
+                low=mid+1;
             }
-            else
-            {
-                right=mid-1;
+            else{
+                high=mid-1;
             }
         }
         return -1;
     }
+
+//recursive one
+public:
+    int binarySearch(vector<int>&nums,int low,int high,int target)
+    {
+        if(low>high) return -1;
+        int mid=(low+high)/2;
+        if(nums[mid]==target)
+        {
+            return mid;
+        }
+        else if(nums[mid]<target) 
+        {
+            return binarySearch(nums,mid+1,high,target);
+        }
+        else{
+            return binarySearch(nums,low,mid-1,target);
+        }
+    }
+    int searchRecursion(vector<int>&nums,int target)
+    {
+        return binarySearch(nums,0,nums.size()-1,target);
+    }    
 };
+
 
 int main()
 {
     vector<int>nums={-1,0,3,5,9,12};
     int target = 3;
 
-    int result=Solution().search(nums,target);
+    int result=Solution().searchRecursion(nums,target);
 
     cout<<result<<endl;
     return 0;
