4f3acd1776321da837a093ffb2ac1a1710045f37
Rimon <mdraisul.i.rimon@gmail.com>
2025-04-06 15:33:10 +0600

    https://leetcode.com/problems/search-in-rotated-sorted-array-ii/description/

---
+++ 81-Search_in_Rotated_Sorted_Array_II.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return true;
+            }
+
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
+                low = low + 1;
+                high = high - 1;
+                continue;
+            }
+
+            // left sorted
+            if (nums[low] <= nums[mid]) {
+                if (nums[low] <= target && target <= nums[mid]) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            // right sorted
+            else {
+                if (nums[mid] <= target && target <= nums[high]) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+        }
+
+        return false;
+    }
+};
+
+int main()
+{
+    vector<int>vec={2,5,6,0,0,1,2};
+    int target=15;
+    
+    int result=Solution().search(vec,target);
+
+    if(result==1) cout<<"True"<<endl;
+    else cout<<"False"<<endl;
+}
