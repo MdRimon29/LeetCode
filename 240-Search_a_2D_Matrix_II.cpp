5d2695a5f832f13cb9650edc130841423321c7e6
Rimon <mdraisul.i.rimon@gmail.com>
2025-04-14 12:28:05 +0600

    https://leetcode.com/problems/search-a-2d-matrix-ii/

---
+++ 240-Search_a_2D_Matrix_II.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//time complexity is O(n+m)
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix.size(), m = matrix[0].size();
+        int row = 0, col = m - 1;
+
+        while (row < n && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] < target) {
+                row += 1;
+            } else {
+                col -= 1;
+            }
+        }
+
+        return false;
+    }
+};
+
+int main()
+{
+    vector<vector<int>>vec={{1,4,7,11,15},
+                            {2,5,8,12,19},
+                            {3,6,9,16,22},
+                            {10,13,14,17,24},
+                            {18,21,23,26,30}};
+    int target=23;
+
+    bool result=Solution().searchMatrix(vec,target);
+    if(result==1)
+    {
+        cout<<"true"<<endl;
+    }
+    else{
+        cout<<"false"<<endl;
+    }
+
+    return 0;
+}
